394fd061132508332d51c159581cba440b2d094c
3ulalia <179992797+3ulalia@users.noreply.github.com>
2026-01-13 23:46:48 -0500

    add neotree and one bind

--- nvim/plugin/neotree.lua
+++ nvim/plugin/neotree.lua
@@ -4,6 +4,11 @@ end
 vim.g.did_load_neotree_plugin = true
 
 -- Highlights unique characters for f/F and t/T motions
-require('neotree').setup {
+require('neo-tree').setup {
   close_if_last_window = true,
 }
+
+vim.keymap.set("n", "<leader><Tab>", "<Cmd>Neotree toggle reveal<CR>")
+vim.api.nvim_create_autocmd("VimEnter", {
+  command = "Neotree show reveal",
+})
