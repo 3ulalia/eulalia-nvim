d300154066f29ed00ecf7fca5c8bb6968e809a59
3ulalia <179992797+3ulalia@users.noreply.github.com>
2026-01-14 16:22:46 -0500

    fix neotree being annoying when i don't want it to

--- nvim/plugin/neotree.lua
+++ nvim/plugin/neotree.lua
@@ -6,9 +6,7 @@ vim.g.did_load_neotree_plugin = true
 -- Highlights unique characters for f/F and t/T motions
 require('neo-tree').setup {
   close_if_last_window = true,
+  hijack_netrw_behavior = "disabled"
 }
 
 vim.keymap.set("n", "<leader><Tab>", "<Cmd>Neotree toggle reveal<CR>")
-vim.api.nvim_create_autocmd("VimEnter", {
-  command = "Neotree show reveal",
-})
