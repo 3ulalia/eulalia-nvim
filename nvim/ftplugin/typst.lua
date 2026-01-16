60533ab72527d356ca22d8faca6d0a85922c7711
3ulalia <179992797+3ulalia@users.noreply.github.com>
2026-01-16 11:15:45 -0500

    add imgclip binds

--- nvim/ftplugin/typst.lua
+++ nvim/ftplugin/typst.lua
@@ -66,17 +66,21 @@ vim.lsp.start {
   },
 }
 
-vim.keymap.set('n', '<leader>tp', '<CMD>TypstTogglePreview<CR>', { desc = '[t]ypst: toggle [p]review' })
-vim.keymap.set('n', '<leader>tc', '<CMD>TypstPreviewFollowCursorToggle<CR>', { desc = '[t]ypst: toggle [c]ursor following' })
-vim.keymap.set('n', '<leader>ts', '<CMD>TypstPreviewSyncCursor<CR>', { desc = '[t]ypst: [s]ync cursor position' })
-
-require("img-clip").setup({
-
-})
-
 require("typst-preview").setup({
   dependencies_bin = {
     ['tinymist'] = "tinymist",
     ['websocat'] = "websocat",
   },
 })
+
+vim.keymap.set('n', '<leader>tv', '<CMD>TypstPreviewToggle<CR>', { desc = '[t]ypst: toggle pre[v]iew' })
+vim.keymap.set('n', '<leader>tc', '<CMD>TypstPreviewFollowCursorToggle<CR>', { desc = '[t]ypst: toggle [c]ursor following' })
+vim.keymap.set('n', '<leader>ts', '<CMD>TypstPreviewSyncCursor<CR>', { desc = '[t]ypst: [s]ync cursor position' })
+
+require("img-clip").setup({
+  default = {
+    file_name = "$FILE_NAME",
+  }
+})
+
+vim.keymap.set('n', '<leader>tp', '<CMD>PasteImage', { desc = '[t]ypst: [p]aste image from clipboard' })
